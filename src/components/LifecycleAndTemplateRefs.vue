2f9df21887834a92e584e872aaa4ee4b85c25a69
Minhaz <minhaz1217@gmail.com>
2022-08-21 23:07:49 +0600

    mounted and watchers

---
+++ src/components/LifecycleAndTemplateRefs.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { onMounted, onUnmounted, ref } from 'vue'
+
+const p = ref(null);
+
+onMounted( ()=>{
+    p.value.textContent = "Hello World";
+    // console.log(p.value);
+});
+onUnmounted(()=>{
+    console.log("Component Unmounted");
+});
+</script>
+
+<template>
+  <p ref="p">hello</p>
+</template>
