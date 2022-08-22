8cde5b3ae607f44ce14771f65b34fde25181056f
Minhaz <minhaz1217@gmail.com>
2022-08-22 23:04:10 +0600

    child components with props and emits

---
+++ src/components/ChildComponentWithEmits.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+
+const emit = defineEmits(['response']);
+
+const childMsg = ref('No child msg yet')
+emit('response', "Hello From the Child");
+</script>
+
+<template>
+  <p>{{ "Child Mounted" }}</p>
+</template>
